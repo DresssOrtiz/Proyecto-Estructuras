b6ca1427bee67f00b2b8d6c3ce9bd66cd790e81e
DresssOrtiz <134727739+DresssOrtiz@users.noreply.github.com>
2025-03-20 10:17:54 -0500

    Update volumen.cpp
    Correccion para evitar copias innecesarias

--- Codigo/volumen.cpp
+++ Codigo/volumen.cpp
@@ -2,7 +2,7 @@
 
 // Métodos de la clase Volumen
 string Volumen::obtenerNombre_volumen() { return nombre_volumen; }
-vector<Imagen> Volumen::obtenerImagenes() { return imagenes; }
+const vector<Imagen>& Volumen::obtenerImagenes() { return imagenes; }
 vector<Imagen> Volumen::obtenerProyecciones() { return proyecciones; }
 void Volumen::fijarNombre_volumen(string xNombre_volumen) { nombre_volumen = xNombre_volumen; }
 void Volumen::fijarImagenes(const vector<Imagen>& xImagenes) { imagenes = xImagenes; }
