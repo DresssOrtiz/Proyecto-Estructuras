819672a6f89895cd50c049b44b34fff84a09279c
DresssOrtiz <134727739+DresssOrtiz@users.noreply.github.com>
2025-04-12 17:47:29 -0500

    Update ArbolHuffman.h
    revise el TAD y quite comentarios innecesarios

--- src/C++/resources/ArbolHuffman.h
+++ src/C++/resources/ArbolHuffman.h
@@ -10,8 +10,8 @@ using namespace std;
 class ArbolHuffman {
 private:
     // Datos Mínimos
-    NodoHuffman* raiz;               // raíz del árbol de Huffman
-    vector<string> codigos;          // vector de códigos Huffman por intensidad
+    NodoHuffman* raiz;              
+    vector<string> codigos;          
 
     // Función auxiliar (privada)
     void generarCodigosAux(NodoHuffman* nodo, string camino);
@@ -21,14 +21,14 @@ public:
     ArbolHuffman();
 
     // Operaciones
-    NodoHuffman* obtenerRaiz();                            // da la raíz del árbol
-    vector<string> obtenerCodigos();                       // da el vector de códigos
-    void fijarRaiz(NodoHuffman* xRaiz);                    // asigna la raíz
-    void fijarCodigos(vector<string> xCodigos);            // asigna el vector de códigos
-    void construirDesdeFrecuencias(unsigned long xFrecuencias[], int tamano); // construye el árbol
-    void generarCodigos();                                 // genera códigos y llena el vector
-    string obtenerCodigo(int xIntensidad);                 // da el código de una intensidad
-    vector<vector<int>> decodificarBits(string xBits, int xAncho, int xAlto); // reconstruye la imagen
+    NodoHuffman* obtenerRaiz();                            
+    vector<string> obtenerCodigos();                       
+    void fijarRaiz(NodoHuffman* xRaiz);                   
+    void fijarCodigos(vector<string> xCodigos);            
+    void construirDesdeFrecuencias(unsigned long xFrecuencias[], int tamano); 
+    void generarCodigos();                                 
+    string obtenerCodigo(int xIntensidad);                 
+    vector<vector<int>> decodificarBits(string xBits, int xAncho, int xAlto); 
 };
 
 #endif
