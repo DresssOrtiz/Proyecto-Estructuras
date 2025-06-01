8ae677af51ab8c92df4ef4c77f6e8ea8feab4474
DresssOrtiz <134727739+DresssOrtiz@users.noreply.github.com>
2025-05-31 20:37:44 -0500

    Update Segmentador.h
    falta revision

--- src/C++/resources/Segmentador.h
+++ src/C++/resources/Segmentador.h
@@ -1,1 +1,36 @@
+#ifndef SEGMENTADOR_H
+#define SEGMENTADOR_H
+
+#include <vector>
+#include <tuple>
+#include <string>
+#include "GrafoImagen.h"
+
+using namespace std;
+
+class Segmentador {
+private:
+    // Datos mínimos
+    GrafoImagen grafo;
+    vector<tuple<int, int, int> > semillas;
+    vector<vector<int> > etiquetas;
+
+public:
+    // Constructor
+    Segmentador();
+
+    // Operaciones
+    void fijarGrafo(GrafoImagen xGrafo);
+    void fijarSemillas(const vector<tuple<int, int, int> >& xSemillas);
+    void fijarEtiquetas(const vector<vector<int> >& xEtiquetas);
+
+    GrafoImagen obtenerGrafo() const;
+    vector<tuple<int, int, int> > obtenerSemillas() const;
+    vector<vector<int> > obtenerEtiquetas() const;
+
+    void segmentar();
+    void guardarComoPGM(const string& nombreArchivo);
+};
+
+#endif
 
